019c5157ca3265a80b7ae8f8f1948b73881951dc
SwumPyaeSone <swumpyaealax@gmail.com>
2025-08-09 01:50:34 +0100

    Remove /register route entirely (internal-only app)

--- frontend/src/app/register/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { redirect } from 'next/navigation';
-
-export default function RegisterPage() {
-  // Registration is disabled for internal-use only
-  redirect('/login');
-} 
